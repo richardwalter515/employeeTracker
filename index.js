151b6f3ba0316f573efd12fb41114584c831f09c
Richard Walter <richardwalter515@gmail.com>
2020-11-24 15:39:52 -0500

    added add dept function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
+
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
@@ -7,11 +9,13 @@ const connection = mysql.createConnection({
   password: 'rootpassword',
   database: 'employee_management_db',
 });
+
 connection.connect((err) => {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   runSearch();
 });
+
 const runSearch = () => {
   inquirer
     .prompt({
@@ -28,5 +32,50 @@ const runSearch = () => {
         'Update an employee role',
         'exit',
       ],
+    }).then((answer) => {
+      switch (answer.action) {
+        case 'Add a department':
+          addDept();
+          break;
+        case 'Add a role':
+          multiSearch();
+          break;
+        case 'Add an employee':
+          rangeSearch();
+          break;
+        case 'View employees by department':
+          songSearch();
+          break;
+        case 'View employees by roles':
+          songSearch();
+          break;
+        case 'View all employees':
+          songSearch();
+          break;
+        case 'Update an employee role':
+          songSearch();
+          break;
+        case 'exit':
+          connection.end();
+          break;
+      }
+    });
+};
+
+const addDept = () => {
+  inquirer
+    .prompt({
+      name: 'deptName',
+      type: 'input',
+      message: 'What department would you like to add?',
     })
-}
+    .then((answer) => {
+      const query = connection.query(
+        "INSERT INTO department SET ?", {name: answer.deptName,},
+        function(err, res) {
+          if (err) throw err;
+          console.log(`successfully added ${answer.deptName} to database`);
+          runSearch();
+        });
+    });
+};
